3f469af2b9a99a40e49cd51698a75511b22ab1cc
MichaelCubriel <michaelcubriel@gmail.com>
2024-09-16 19:19:23 -0500

    API interface added

---
+++ src/api.py
@@ -0,0 +1,25 @@
+import asyncio
+
+"""
+FOR AI
+'train_model'
+'prediction_run_state'
+'prediction_run(interval, timeout)'
+
+FOR CLASSIFICATION
+'averaging_interval_peiod'
+
+FOR API
+'device_info'
+'active_tasks'
+'subscribe(interrval, timeout)'
+'unsubscibe'
+
+
+"""
+class predictAPI():
+    def __init__(self):
+        var = None:
+    
+    async def command(self, message) -> str:
+        
